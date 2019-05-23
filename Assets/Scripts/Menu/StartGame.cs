75d178d2922d7e2e9cc25c10a8ca7e527eb7f819
pakaratAriya <pakarat_ariya@hotmail.com>
2019-05-23 16:34:58 -0700

    create reset button to reset gems and coin

--- Assets/Scripts/Menu/StartGame.cs
+++ Assets/Scripts/Menu/StartGame.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class StartGame : MonoBehaviour {
     [SerializeField]
@@ -11,5 +12,19 @@ public class StartGame : MonoBehaviour {
 	SceneManager.LoadScene(levelName);
 	MapManager.ResetValue();
 	PoolManager.ResetValue();
+    }
+
+    public void ResetValue(){
+        PlayerPrefs.SetInt("Coins", 0);
+        string[] scenes = new string[SceneManager.sceneCountInBuildSettings];
+        for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++){
+	    scenes[i] = Path.GetFileNameWithoutExtension(SceneUtility.GetScenePathByBuildIndex(i));
+        }
+        for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
+	for(int j = 1; j <= 3; j++){
+            string sName = "Gem-" + scenes[i] + j;
+            PlayerPrefs.SetInt(sName, 0);
+        }
+	    
     }
 }
