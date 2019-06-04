9c6878d7651e8624123469a1322acec8594f2821
pakaratAriya <pakarat_ariya@hotmail.com>
2019-06-04 14:20:25 -0700

    create gameplayUI script

---
+++ Assets/Scripts/InGameUI/GamePlayUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePlayUI : MonoBehaviour
+{
+    public GameObject PauseMenu;
+    public void PauseGame()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void ResumeGame()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
