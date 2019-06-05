122394860075b1d39694341db6c92f94a4be8769
pakaratAriya <pakarat_ariya@hotmail.com>
2019-06-04 17:13:25 -0700

    create a level select scene

--- Assets/Scripts/Menu/LevelName.cs
+++ Assets/Scripts/Menu/LevelName.cs
@@ -1,8 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelName : MonoBehaviour
 {
-    
+    private string name;
+    public Text levelTxt;
+    private int levelIndex;
+    [SerializeField]
+    private Image[] gemUIs = new Image[3];
+    public Sprite emptySpr;
+    public Sprite gemSpr;
+
+    public void SetGem()
+    {
+        for (int i = 1; i <= 3; i++)
+        {
+            string gemName = "Gem-" + name + i;
+            if (PlayerPrefs.GetInt(gemName) != 0)
+            {
+                gemUIs[i - 1].sprite = gemSpr;
+            }
+        }
+        
+    }
+
+    public void SetLevelName(string n, int i)
+    {
+        name = n;
+        levelIndex = i + 1;
+        levelTxt.text = "" + levelIndex;
+        SetGem();
+    }
+    public string GetLevelName()
+    {
+        return name;
+    }
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(name);
+    }
 }
--- Assets/Scripts/Menu/LevelSelect.cs
+++ Assets/Scripts/Menu/LevelSelect.cs
@@ -1,9 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelSelect : MonoBehaviour
 {
-    List<string> levels = new List<string>();
+    public List<string> levels = new List<string>();
+    public LevelName levelUiPref;
+    private Canvas canvas;
     
+
+    private void Awake()
+    {
+        canvas = FindObjectOfType<Canvas>();
+        for(int i = 0; i < levels.Count; i++)
+        {
+            LevelName lvl = Instantiate<LevelName>(levelUiPref);
+            lvl.SetLevelName(levels[i], i);
+            Image img = lvl.GetComponent<Image>();
+            img.transform.parent = canvas.transform;
+            img.rectTransform.localPosition = new Vector2(-140 + (70 * (i % 5)), 160 - (80 * (i / 5)));
+            
+        }
+    }
 }
